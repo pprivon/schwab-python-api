c77281bc89164564695bd21f37ab73f7653164a6
sgttux <116453812+sgttux@users.noreply.github.com>
2024-07-03 15:23:12 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='schwab_python_api',
-    version='0.1.1',
+    version='0.1.2',
     packages=['schwab_python_api',],
     #long_description=open('README.md').read(),
 )
